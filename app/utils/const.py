d44f37a148db4a826a1bac9f78fa46fa66c6644a
Maxime Dréan <91475935+maximedrn@users.noreply.github.com>
2022-05-07 17:37:29 +0200

    Update const.py

--- app/utils/const.py
+++ app/utils/const.py
@@ -25,7 +25,7 @@ FIRST_PAGE = (
     '\n\nCopyright © 2022 Maxime Dréan. All rights reserved.'
     '\nAny distribution, modification or commercial use '
     'is strictly prohibited.'
-    f'\n\nVersion 1.7.5 - 2022, 07 May.{RESET}'
+    f'\n\nVersion 1.7.6 - 2022, 07 May.{RESET}'
     '\n\nIf you face any problem, please open an issue.')
 
 ENTER = '\nPRESS [ENTER] TO CONTINUE. '
