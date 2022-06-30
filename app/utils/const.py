91816f7adc370e9ebe50ff45098cd0616130c5ca
Maxime Dréan <91475935+maximedrn@users.noreply.github.com>
2022-06-30 19:58:56 +0200

    Update const.py

--- app/utils/const.py
+++ app/utils/const.py
@@ -18,7 +18,7 @@ from .colors import GREEN, YELLOW, RESET
 from .user import check_version
 
 
-VERSION = '1.9.8'
+VERSION = '1.10.0'
 
 # Change this with any values.
 # 1 means 2 failures allowed.
