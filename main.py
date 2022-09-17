968bb5f27d32c95dde15b84e52714a80cf0908a9
Maxime <91475935+maximedrn@users.noreply.github.com>
2022-09-17 11:31:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -73,7 +73,8 @@ def process(action: list, solver: int, key: str, structure: object,
         delete.init(structure, web)
     # Proceed to the Upload or Sale process in a loop.
     for nft_number in range(starting, reader.lenght_file):
-        if nft_number % 10 == 0:  # Every 10 NFTs, wait a few seconds.
+        # Every 10 NFTs (except 10 first), wait a few seconds.
+        if nft_number % 10 == 0 and nft_number != starting:
             web.driver.get('data:,')  # Go to an empty page and wait
             sleep(randint(2, 5))  # between 2 and 5 seconds to ease the RAM.
         print(f'\nNFT n°{nft_number + 1}/{reader.lenght_file}:')
