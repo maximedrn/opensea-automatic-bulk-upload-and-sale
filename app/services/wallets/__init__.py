5e560e69cfdf51ce7cfc29bec18e5dd3fef0a4de
Maxime <91475935+maximedrn@users.noreply.github.com>
2023-04-28 13:21:01 +0200

    Update __init__.py

--- app/services/wallets/__init__.py
+++ app/services/wallets/__init__.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python
 # app/services/wallets/__init__.py
+
+
+def extract_zip(path: str) -> str:
+    """Extract the MetaMask ZIP content."""
+    from zipfile import ZipFile
+    from os.path import exists
+    folder: str = path.replace('.crx', '')
+    if not exists(folder):
+        with ZipFile(path, 'r') as zip_content:
+            zip_content.extractall(folder)
+    return folder
