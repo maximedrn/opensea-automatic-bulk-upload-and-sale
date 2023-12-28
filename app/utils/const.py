5f8bbb086ae96ce695c96011d5ee835da3cd3951
Maxime <maximedrean@orange.fr>
2023-12-28 18:30:09 +0100

    feat: last commit

--- app/utils/const.py
+++ app/utils/const.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+# -*- coding: utf-8 -*-
 # app/utils/const.py
 
 
@@ -7,9 +7,6 @@
 
 Github: https://github.com/maximedrn
 Telegram: https://t.me/maximedrn
-
-Copyright © 2023 Maxime Dréan. All rights reserved.
-Any distribution, modification or commercial use is strictly prohibited.
 """
 
 
@@ -30,27 +27,13 @@ FIRST_PAGE = (
     f'{GREEN}Created by Maxime Dréan.'
     '\nGithub: https://github.com/maximedrn'
     '\nTelegram: https://t.me/maximedrn'
-    '\nYouTube: https://www.youtube.com/channel/UCoqpR1OLb'
-    'swIyQVatKBoGxA'
-    '\nEthereum: 0xDD135d5be0a23f6daAAE7D2d0580828c9e09402E'
-    '\n\nCopyright © 2023 Maxime Dréan. All rights reserved.'
-    '\nAny distribution, modification or commercial use '
-    'is strictly prohibited.'
-    f'\n\nVersion {VERSION} - 2023, 29 August.{RESET}'
+    f'\n\nVersion {VERSION} - 2023, 06 December.{RESET}'
     f'{check_version(VERSION)}'  # Check for a new update of the bot.
-    '\n\nAdd-ons available here: https://maximedrn.gumroad.com/'
-    '\nJOIN ME ON TELEGRAM: https://t.me/opensea_bulk_upload_and_sale'
     '\n\nIf you face any problem, please open an issue.')
 
 ENTER = '\nPRESS [ENTER] TO CONTINUE. '
 
-SECOND_PAGE = (
-    f'{GREEN}Created by Maxime Dréan.'
-    '\nCopyright © 2023 Maxime Dréan. All rights reserved.'
-    '\nAny distribution, modification or commercial use '
-    f'is strictly prohibited.{RESET}'
-    '\n\nAdd-ons available here: https://maximedrn.gumroad.com/'
-    '\nJOIN ME ON TELEGRAM: https://t.me/opensea_bulk_upload_and_sale')
+SECOND_PAGE = f'{GREEN}Created by Maxime Dréan.'
 
 STARTING_VALUE = '\nStart from the item n°'
 
@@ -89,13 +72,4 @@ YOLO_ERROR = (
     '\nevery modules are installed and files are correctly '
     f'\ndownloaded and presents in the right directory.\n{RESET}')
 
-NO_CAPTCHA_ERROR = (
-    'Cannot found the file. Please check that you placed it in the\n'
-    'app/services/solvers/no_captcha.py path or that you purchased'
-    '\nthis extension. (available here: '
-    'https://maximedrn.gumroad.com/l/opensea-no-recaptcha)')
-
-ALL_DONE = (
-    f'\n{GREEN}All done!{RESET}'
-    '\nIn order to support me, you can make donations at this address:'
-    '\n0xDD135d5be0a23f6daAAE7D2d0580828c9e09402E (Ethereum).')
+ALL_DONE = f'\n{GREEN}All done!{RESET}'
